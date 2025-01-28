ed3e755057366d497863520d2b58969eff7aa34d
doxart <gokdeniz.gs41@gmail.com>
2025-01-29 02:49:29 +0300

    Initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,18 +30,20 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
 dependencies {
+    //implementation("com.github.doxart:easypops:1.0.0") from jitpack
+
+    implementation(project(":easypops")) // from local module
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(project(":easypops"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
