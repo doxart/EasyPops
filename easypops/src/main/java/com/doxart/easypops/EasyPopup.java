685028c8e6e9ef832c1a010f5534a106775bae6b
doxart <gokdeniz.gs41@gmail.com>
2025-09-21 20:12:14 +0300

    Initial

--- easypops/src/main/java/com/doxart/easypops/EasyPopup.java
+++ easypops/src/main/java/com/doxart/easypops/EasyPopup.java
@@ -3,8 +3,14 @@ package com.doxart.easypops;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 
 public class EasyPopup {
+    private static final String TAG = "EasyPopup";
+    private static final String ACTION_CANCEL_PROGRESS_RESULT_NULL_POP = "Current popup is not found!";
+    private static final String ACTION_CANCEL_PROGRESS_RESULT_NOT_SHOWING = "Current popup is not showing!";
+    private static final String ACTION_CANCEL_PROGRESS_RESULT_SUCCESS = "Success!";
+
     private static Dialog currentPop;
 
     protected static void setCurrentPop(Dialog currentPop) {
@@ -17,12 +23,25 @@ public class EasyPopup {
     }
 
     protected static void clearCurrentPop() {
-        if (currentPop != null && currentPop.isShowing()) currentPop.cancel();
         currentPop = null;
     }
 
     public static Dialog getCurrentPop() {
+        if (currentPop == null) Log.e(TAG, "getCurrentPop: ", new Exception(ACTION_CANCEL_PROGRESS_RESULT_NULL_POP));
         return currentPop;
+    }
+    public static String cancelCurrentPop() {
+        if (currentPop == null) {
+            Log.e(TAG, "cancelCurrentPop: ", new Exception(ACTION_CANCEL_PROGRESS_RESULT_NULL_POP));
+            return ACTION_CANCEL_PROGRESS_RESULT_NULL_POP;
+        }
+        if (!currentPop.isShowing()) {
+            Log.e(TAG, "cancelCurrentPop: ", new Exception(ACTION_CANCEL_PROGRESS_RESULT_NOT_SHOWING));
+            return ACTION_CANCEL_PROGRESS_RESULT_NOT_SHOWING;
+        }
+        currentPop.cancel();
+        clearCurrentPop();
+        return ACTION_CANCEL_PROGRESS_RESULT_SUCCESS;
     }
 
     //Progress Start
